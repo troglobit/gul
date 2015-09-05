edd23659c460f7604c4f76a0d2604d2a632b15d7
Joachim Nilsson <troglobit@gmail.com>
2015-09-05 21:31:59 +0200

    main.c: Minor cleanup and addtion of SIGTERM handler.
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/main.c
+++ src/main.c
@@ -38,57 +38,73 @@
 #include "plugin.h"
 
 /* Liten fånig text för att retas med den som läser i binären... :) */
-char teaser[] = "Nyfiken gul? (Which is swedish for: \"Curious yellow?\" ;-)";
+char teaser[] = "Nyfiken gul? (Swedish for: \"Curious yellow?\" ;-)";
 
-/* The name the program was run with, stripped of any leading path. */
-char *program_name;
-
-/* getopt_long return codes */
-enum { DUMMY_CODE = 129
-};
-
-/* Option flags and variables */
-
-int want_verbose;		/* --verbose */
-
-static struct option const long_options[] = {
-	{"verbose", no_argument, 0, 'v'},
-	{"help", no_argument, 0, 'h'},
-	{"version", no_argument, 0, 'V'},
-	{NULL, 0, NULL, 0}
-};
-
-/*** Prototypes *********************************************************
- */
-static void ignore(int sig);
-
-static void finish(int sig);
-static void usage(int status);
-static int decode_switches(int argc, char **argv);
+extern char *__progname;
 
 
+static void ignore(int sig)
+{
+	LOG("Ignored signal %d\n", sig);
+}
+
+static void finish(int sig)
+{
+	pluginFinish();
+	exit(0);
+}
+
+static int usage(int status)
+{
+	printf("Usage: %s [OPTIONS] [FILE]\n", __progname);
+	printf("Options:\n"
+               "      --verbose     Verbose logging\n"
+               "  -h, --help        This help text\n"
+               "  -v, --version     Show version information\n"
+               "=============================================\n");
+	printf("%s - The one true GUL editor!\n", __progname);
+
+	return status;
+}
 
 int main(int argc, char *argv[])
 {
-	int i, quit = 0;
+	int c, quit = 0;
 	buffer_t *currentBuffer;
 	char *fileName = NULL;
 	keyevent_t thisEvent;
+        struct option long_options[] = {
+           {"verbose", no_argument, 0, 'V'},
+           {"help",    no_argument, 0, 'h'},
+           {"version", no_argument, 0, 'v'},
+           {NULL, 0, NULL, 0}
+        };
 
-	program_name = argv[0];
+	while ((c = getopt_long(argc, argv, "vhv", long_options, (int *)0)) != EOF) {
+		switch (c) {
+		case 'v':
+			printf("gul %s\n", VERSION);
+			return 0;
 
-	i = decode_switches(argc, argv);
+		case 'h':
+			return usage(0);
 
-	if (argv[i])
-		fileName = strDup(argv[i]);
-
-	currentBuffer = loadFile(fileName);
-
-	/* openDisplay(); */
-	createScreen();
+		default:
+			return usage(EXIT_FAILURE);
+		}
+	}
 
 	/* Take care of Ctrl-C */
 	signal(SIGINT, ignore);
+	signal(SIGTERM, finish);
+
+	if (argv[optind])
+		fileName = strDup(argv[optind]);
+
+	currentBuffer = loadFile(fileName);
+
+	/* Main screen turn on ... */
+	createScreen();
 
 	/* Debugging sent to /dev/null or stderr ... */
 #if DEBUG == 0
@@ -119,30 +135,12 @@ int main(int argc, char *argv[])
 	freeBuffer(currentBuffer);
 	pluginFinish();
 
-	return (0);
+	return 0;
 }
-
-static void ignore(int sig)
-{
-	LOG("Ignored signal %d\n", sig);
-}
-
-
-static void finish(int sig)
-{
-	pluginFinish();
-
-	exit(0);
-}
-
-
 
 void trySave(buffer_t *buf)
 {
 	if (!buf->filename) {
-		/* #include <unistd.h>
-		   getcwd (str, 80);
-		 */
 		int y;
 		y = display_status(buf, "Save As: ");
 		buf->filename = read_string(9, y);
@@ -156,63 +154,12 @@ buffer_t *tryLoad(buffer_t *buf)
 	int y;
 	char *fileName;
 
-	/* #include <unistd.h>
-	   getcwd (str, 80);
-	 */
 	y = display_status(buf, "Load file: ");
 	fileName = read_string(11, y);
 
 	freeBuffer(buf);
 	return loadFile(fileName);
 }
-
-
-/* Set all the option flags according to the switches specified.
-   Return the index of the first non-option argument.  */
-
-static int decode_switches(int argc, char **argv)
-{
-	int c;
-
-
-	while ((c = getopt_long(argc, argv, "v"	/* verbose */
-				"h"	/* help */
-				"V",	/* version */
-				long_options, (int *)0)) != EOF) {
-		switch (c) {
-		case 'v':	/* --verbose */
-			want_verbose = 1;
-			break;
-		case 'V':
-			printf("gul %s\n", VERSION);
-			exit(0);
-
-		case 'h':
-			usage(0);
-
-		default:
-			usage(EXIT_FAILURE);
-		}
-	}
-
-	return optind;
-}
-
-
-static void usage(int status)
-{
-	printf("%s - The one true GUL editor!\n", program_name);
-	printf("Usage: %s [OPTION]... [FILE]...\n", program_name);
-	printf("\
-Options:\n\
-  --verbose                  print more information\n\
-  -h, --help                 display this help and exit\n\
-  -V, --version              output version information and exit\n\
-");
-
-	exit(status);
-}
-
 
 /**
  * Local Variables:
